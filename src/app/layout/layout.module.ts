94edffad6e9aee71fa96aee3fdf517d4a2a1f08a
Jim Skogman <yim.apichai@gmail.com>
2017-04-19 17:27:21 +0700

    Add navbar and sidebar components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NavbarComponent, SidebarComponent]
 })
 export class LayoutModule { }
