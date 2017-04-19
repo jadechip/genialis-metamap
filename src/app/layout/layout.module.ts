2fcff81f529402aca0fbc4a0f7f6a1f6fa0825a9
Jim Skogman <yim.apichai@gmail.com>
2017-04-19 17:34:44 +0700

    Add layout routing file

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutRouting } from "./layout.routing";
+
 import { LayoutComponent } from "./layout.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutRouting
   ],
   declarations: [LayoutComponent, NavbarComponent, SidebarComponent]
 })
---
+++ src/app/layout/layout.routing.ts
@@ -0,0 +1,6 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const LAYOUT_ROUTES: Routes = [];
+
+export const LayoutRouting = RouterModule.forChild(LAYOUT_ROUTES);
