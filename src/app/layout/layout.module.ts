0722d4fe05f4b2095c6130fb7bbebfd16252182c
Jim Skogman <yim.apichai@gmail.com>
2017-04-19 17:31:30 +0700

    Add layout component to layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from "./layout.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
@@ -7,6 +8,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent, SidebarComponent]
+  declarations: [LayoutComponent, NavbarComponent, SidebarComponent]
 })
 export class LayoutModule { }
