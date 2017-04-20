69d132f1407ded1bf067dfcdf27fc8dc5c0ad08d
Jim Skogman <yim.apichai@gmail.com>
2017-04-20 12:40:52 +0700

    Add sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate} from '@angular/core';
+// import { SharedService } from "../../shared/services/shared.service";
 
 @Component({
   selector: 'metamap-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+    animations: [
+        trigger('toggleHeight', [
+            state('inactive', style({
+                height: '0',
+                opacity: '0'
+            })),
+            state('active', style({
+                height: '*',
+                opacity: '1'
+            })),
+            transition('inactive => active', animate('200ms ease-in')),
+            transition('active => inactive', animate('200ms ease-out'))
+        ])
+    ]
 })
+
 export class SidebarComponent implements OnInit {
+    sidebarVisible: boolean;
 
-  constructor() { }
+    // Sub menu visibilities
+    navigationSubState:any = {
+        Tables: 'inactive',
+        Forms: 'inactive',
+        SamplePages: 'inactive',
+        UserInterface: 'inactive',
+        Components: 'inactive',
+        Charts: 'inactive',
+    };
 
-  ngOnInit() {
-  }
+    // Toggle sub menu
+    toggleNavigationSub(menu, event) {
+        event.preventDefault();
+        this.navigationSubState[menu] = (this.navigationSubState[menu] === 'inactive' ? 'active' : 'inactive');
+    }
+
+    constructor() {
+        // pass as parameter -> private sharedService: SharedService
+        // sharedService.sidebarVisibilitySubject.subscribe((value) => {
+        //     this.sidebarVisible = value
+        // })
+    }
+
+    ngOnInit() {
+    }
 
 }
